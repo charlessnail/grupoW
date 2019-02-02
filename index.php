ef649b3baf278e4d17508016157584069fa68577
Carlos Eduardo <carloseducorrea@hotamail.com>
2019-02-01 23:44:42 -0200

    basico para inicio.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!--PG PARA TESTE DO GRUPO, COMO TRABALHAR CADA QUAL EM SUA HABILIDADE-->
+<!DOCTYPE html><!--indica ao navegador que o documento é em HTML5-->
+<html><!---->
+	<head>
+		<title></title>
+		<meta charset="utf-8"/>
+		
+	</head>
+	<body>
+		<h1>GRUPO Zap HTML, CSS, JAVASCRIPT</h1>
+		<label>Nome:</label><br>
+		<input type="text" name="nome"><br>
+		<label>Email:</label><br>
+		<input type="text" name="email"><br>
+		<label>Estado:</label><br>
+		<input type="text" name="estado"><br>
+		
+		<input type="submit" value="Gravar"><br>
+
+
+	</body>
+</html>
